10eed96f8cc5a0d0e8b37fc6c6f72057f87727b1
WEN\wenbo <wenxx194@d.umn.edu>
2018-02-10 12:54:16 -0600

    Front end Android

---
+++ Hmail/app/src/main/java/com/example/bo/hmail/Home.java
@@ -0,0 +1,19 @@
+package com.example.bo.hmail;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by wenbo on 2/8/2018.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
